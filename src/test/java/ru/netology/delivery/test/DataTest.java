fa174be5b9a02f05b58b9bc6bb116dcaa9ddcdec
Марина Безотеческая <bezotecheskaya@mail.ru>
2022-09-06 00:09:50 +0700

    "Allure"

---
+++ src/test/java/ru/netology/delivery/test/DataTest.java
@@ -0,0 +1,65 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.commands.Click;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.SendKeysAction;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+
+class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    public void openBrowser() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    public void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $x("//input[@name='name']").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue("+79131110022");
+        $("[data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//*[contains(text(),'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").setValue(secondMeetingDate);
+        $x("//span[@class='button__text']").click();
+        $("[data-test-id='replan-notification'] .button__text").click();
+        $x("//*[contains(text(),'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
